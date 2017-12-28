589b9b3033f7dc2ebf201fd693f9407f502883c6
androidlike <112326190@qq.com>
2017-12-28 23:12:03 +0800

    change toast

--- first/src/main/java/com/scts/hand/first/CDialogMethod.java
+++ first/src/main/java/com/scts/hand/first/CDialogMethod.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.widget.Toast;
 
 /**
  * Created by cf on 2017/12/28.
@@ -13,16 +14,18 @@ public class CDialogMethod {
 
     public static void showDialog(Activity  activity){
 
-        AlertDialog.Builder  builder=new AlertDialog.Builder(activity);
+      /*  AlertDialog.Builder  builder=new AlertDialog.Builder(activity);
         builder.setTitle("first kotlin");
         builder.setMessage("success show dialog!");
+
         builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                     dialog.dismiss();
             }
         });
-        builder.create().show();
+        builder.create().show();*/
+        Toast.makeText(activity,"hello world! success show dialog!",Toast.LENGTH_SHORT).show();
 
 
     }
