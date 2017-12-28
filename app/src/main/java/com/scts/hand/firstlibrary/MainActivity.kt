94dc5825f86cd7a1db08398109bb539e690213b2
androidlike <112326190@qq.com>
2017-12-28 22:43:13 +0800

    修改library

--- app/src/main/java/com/scts/hand/firstlibrary/MainActivity.kt
+++ app/src/main/java/com/scts/hand/firstlibrary/MainActivity.kt
@@ -2,11 +2,22 @@ package com.scts.hand.firstlibrary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.scts.hand.first.CDialogMethod
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+     val tv=findViewById<TextView>(R.id.textview_helloword)
+     tv.setOnClickListener {
+            CDialogMethod.showDialog(this)
+        }
+
+
+
     }
 }
--- first/src/main/java/com/scts/hand/first/CDialogMethod.java
+++ first/src/main/java/com/scts/hand/first/CDialogMethod.java
@@ -14,17 +14,16 @@ public class CDialogMethod {
     public static void showDialog(Activity  activity){
 
         AlertDialog.Builder  builder=new AlertDialog.Builder(activity);
-        AlertDialog dialog=builder.create();
-        dialog.setTitle("first library!");
-        dialog.setMessage("success show dialog!");
-        dialog.setButton(1, "确定", new DialogInterface.OnClickListener() {
+        builder.setTitle("first kotlin");
+        builder.setMessage("success show dialog!");
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-
+                    dialog.dismiss();
             }
         });
-        dialog.show();
+        builder.create().show();
+
 
     }
 
